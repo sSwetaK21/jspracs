cbd239bf418204dcdd98e32607163ebeb73e90be
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-17 22:13:55 +0530

    js

--- shweta.js
+++ shweta.js
@@ -222,17 +222,31 @@
 
 // print perfect Number
 
+// let number= parseInt(process.argv[2]);
+// let perfect=0;
+// for(let i=1;i<number;i++){
+//     if(number%i==0){
+//         perfect=perfect+i;
+//     }
+// }
+// if(perfect==number){
+//     console.log("perfect number");
+// }   else{
+//     console.log("not a perfect number");
+// }
+
+
 let number= parseInt(process.argv[2]);
 let perfect=0;
-for(let i=1;i<number;i++){
+let i=1;
+while(i<number){
     if(number%i==0){
         perfect=perfect+i;
     }
+    i++;
 }
 if(perfect==number){
     console.log("perfect number");
-}   else{
+}  else{
     console.log("not a perfect number");
 }
-
-
