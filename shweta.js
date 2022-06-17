0177eefd3e5a47eebf0fd2d71c0b499b95f9a85d
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-17 19:31:50 +0530

    js

--- shweta.js
+++ shweta.js
@@ -13,6 +13,48 @@
 
 // write a program to find the area of a circle
 
-let radius = 5;
-let area = Math.PI*radius*radius;
-console.log(area);
+// let radius = 5;
+// let area = Math.PI*radius*radius;
+// console.log(area);
+
+// write a program to find the area of a rectangle
+
+// let length = 5;
+// let breadth = 6;
+// let area = length*breadth;
+// console.log(area);
+
+// write a program to find the area of a triangle
+
+// let base = 5;
+// let height = 6;
+// let area = (base*height)/2;
+// console.log(area);
+
+// write a program to find the area of a trapezium
+
+// let base1 = 5;
+// let base2 = 6;
+// let height = 7;
+// let area = ((base1+base2)/2)*height;
+// console.log(area);
+
+// write a program to find the area of a square
+
+// let side = 5;
+// let area = side*side;
+// console.log(area);
+
+// write a program to find the area of a cube
+
+// let side = 3;
+// let area = side*side*side;
+// console.log(area);
+
+
+// using looop print all natural numbers
+
+let number=  parseInt(process.argv[2]);
+for(let i=1;i<=number;i++){
+    console.log(i);
+}
