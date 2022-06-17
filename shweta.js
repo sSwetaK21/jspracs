a3dc9ff58be05710093b585f79cfce61c79ce061
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-17 12:43:04 +0530

    js

---
+++ shweta.js
@@ -0,0 +1,18 @@
+
+// write a program to add two numbers
+
+// let a=10;
+// let b= 20;
+// console.log(a+b);
+
+// write a program for square of a number
+
+// let num = 2;
+// let sq = num*num;
+// console.log(sq);
+
+// write a program to find the area of a circle
+
+let radius = 5;
+let area = Math.PI*radius*radius;
+console.log(area);
