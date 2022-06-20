d92b6a5e8dccc79181a8d8d48b5ba1a386520d12
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-20 21:07:52 +0530

    js

---
+++ 20thjune.js
@@ -0,0 +1,58 @@
+//arithmetic operations
+
+// let a =5;
+// a++;
+// console.log(a);
+
+// let b =5;
+// ++b;
+// console.log(b);
+
+// let a =5;
+// a--;
+// console.log(a);
+
+// let b = 5;
+// --b;
+// console.log(b);
+
+
+//while loop
+//  let i=1;
+//     while(i<=10){
+//         console.log(i);
+//         i++;
+//     }
+//     console.log("end of loop");
+
+
+//do while loop
+
+
+// let i=1;
+//     do{
+//         console.log(i);
+//         i++;
+//     }
+//     while(i<=10){
+//         console.log("end of loop");
+//     }
+
+// for loop
+
+//  for(let i=1;i<=10;i++){
+//     console.log(i);
+// } 
+// console.log("end of loop");
+
+// for loop with condition
+
+    // for(let i=1;i<=10;i++){
+    //     if(i%2==0){
+    //         console.log(i + " Even");
+    //     } else{
+    //         console.log("not an even number");
+    //     }
+    // }
+    // console.log("end of loop");
+
