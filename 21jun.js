177c8f7efa9a2e5b73721af8974af9d34b3d225a
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-21 21:40:12 +0530

    js

--- 21jun.js
+++ 21jun.js
@@ -17,3 +17,29 @@
 // }
 // console.log(sumOfSquare(5));
 
+
+// let userName = "John";
+
+// function  greetUser(){
+//     let message = "Hello " + userName;
+//     console.log(message);
+// }
+
+// greetUser();
+
+
+function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  
+  let age = 18;
+  
+  if ( checkAge(age) ) {
+    console.log( 'Access granted' );
+  } else {
+    console.log( 'Access denied' );
+  }
