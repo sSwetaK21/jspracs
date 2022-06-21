1c907acc07609cd5fd8cbc7fb9f47d5f0ffc29cd
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-21 17:01:31 +0530

    js

---
+++ 21jun.js
@@ -0,0 +1,19 @@
+// Arrow functions
+
+
+
+// let add = (a,b) =>{
+//     return a+b;
+// }
+
+// console.log(add(5,6));
+
+// let sumOfSquare = (num) => {
+//     let sum = 0;
+//     for(let i=1; i<=num; i++){
+//         sum += i*i;
+//     }
+//     return sum;
+// }
+// console.log(sumOfSquare(5));
+
