6d555376335f0861c4909ecfd900d6dd2f3d3ee9
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-22 18:27:45 +0530

    js

--- 21jun.js
+++ 21jun.js
@@ -28,18 +28,18 @@
 // greetUser();
 
 
-function checkAge(age) {
-    if (age >= 18) {
-      return true;
-    } else {
-      return confirm('Do you have permission from your parents?');
-    }
-  }
+// function checkAge(age) {
+//     if (age >= 18) {
+//       return true;
+//     } else {
+//       return console.log('Do you have permission from your parents?');
+//     }
+//   }
   
-  let age = 18;
+//   let age = 13;
   
-  if ( checkAge(age) ) {
-    console.log( 'Access granted' );
-  } else {
-    console.log( 'Access denied' );
-  }
+//   if ( checkAge(age) ) {
+//     console.log( 'Access granted' );
+//   } else {
+//     console.log( 'Access denied' );
+//   }
