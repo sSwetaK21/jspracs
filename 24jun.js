6b8a8ec75ff190a5aeeb6f898997e737ea61351c
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-24 16:16:10 +0530

    typeconversion

---
+++ 24jun.js
@@ -0,0 +1,23 @@
+// var key = 0;
+// switch (key) {
+//     case 0:
+//         console.log("case 0");
+//         break;
+//     case 1:
+//         console.log("case 1");
+//         break;
+//     default:
+//         console.log("default");
+//         break;
+// }
+
+// type conversions
+
+let num1 =  23;
+console.log(num1, typeof num1);
+
+let num2 = String(23);
+console.log(num2, typeof num2);
+
+num1 = Number(true);
+console.log(num1, typeof num1);
