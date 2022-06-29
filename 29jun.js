2a3351dc7ae63d3c8079689f637c05944e6d82da
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-29 22:58:51 +0530

    js

--- 29jun.js
+++ 29jun.js
@@ -1,9 +1,37 @@
-let num =4;
+// Javascript Pattern
 
-for(i=1; i<=num; i++){
-  let star = "";
-  for(j=1; j<=num; j++){
-    star += i;
-  }
-  console.log(star);
-}
+
+
+// let num =4;
+
+// for(i=1; i<=num; i++){
+//   let star = "";
+//   for(j=1; j<=num; j++){
+//     star += i;
+//   }
+//   console.log(star);
+// }
+
+// o/p:
+// 1111
+// 2222
+// 3333
+// 4444
+
+
+// let num =4;
+
+// for(i=num; i>=1; i--){
+//   let star = "";
+//   for(j=num; j>=i; j--){
+//     star += "*";
+//   }
+//   console.log(star);
+// }
+
+
+// o/p:
+// *
+// **  
+// *** 
+// ****
