94424fb16b60e7f6227e9237fb5ce5f76e676555
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-07-05 22:15:55 +0530

    js

--- jun5.js
+++ jun5.js
@@ -9,4 +9,18 @@ let country = 'India';
 let continent = 'Asia';
 let population = 3.5;
 
-console.log(country, continent, population)
+// console.log(country, continent, population);
+
+
+// Data Types
+
+// 1. Declare a variable called 'isIsland' and set its value according to your 
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console.
+
+
+let isIsland = true;
+let language;
+console.log(isIsland, country, population, language)
