36d7c37e8edf85f6d8312e3addd12348aa4703fd
shweta <shwetakodamg@gmail.com>
2022-07-11 20:23:43 +0530

    js

--- jun11.js
+++ jun11.js
@@ -12,10 +12,37 @@
 
 
 //-------------Repeating number----------------
+// function isDup(num){
+//     for(let i=0; i<num.length; i++){
+//         if(num.includes(num[i], i+1)){
+//             return true; 
+//         } 
+//     }
+//     return false;
 
-let num=[1,2,3,9,3,2,8,1,5,7,8];
-for(let i=0; i<num.length; i++){
-    if(num.includes(num[i], i+1)){
-        console.log(num[i]); 
-    }
+// }
+// console.log(isDup([1,2,3,4,2]))
+
+// ----delete-----------
+
+// function delDup(arr) {
+//     // let temp=[];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr.includes(arr[i], i + 1)) {
+//             arr.splice(i, 1);
+//         }
+//     }
+//     return arr;
+// }
+// console.log(delDup([1, 2, 2, 3, 4, 4, 5]))
+
+
+//----------missing number-------------
+
+function missingNum(n){
+    let total = Math.floor((n + 1) * (n + 2) / 2);
+        for (let i = 0; i < n; i++)
+            total -= a[i];
+        return total;
 }
+console.log(missingNum([1,2,3,6,7,8,,4]))
