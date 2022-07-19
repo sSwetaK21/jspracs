3c86d96f8f81300bfdf0cb1861eb405c6208367a
shweta <shwetakodamg@gmail.com>
2022-07-19 15:44:32 +0530

    js

---
+++ jun19.js
@@ -0,0 +1,14 @@
+// function division (arr,n){
+//     let count =0
+//     for(let i=0;i<arr.length;i++){
+//         if(i%n==0){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(division([1,5,7,8],1))
+
+
+//=====filter()=========
+// filtered down to just the elements from the given array that pass the test implemented by the provided function.
