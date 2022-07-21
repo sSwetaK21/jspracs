6c122439a4f113bf10ce96e25eacdf3e191806e7
shweta <shwetakodamg@gmail.com>
2022-07-22 00:01:23 +0530

    array methods

--- jun21.js
+++ jun21.js
@@ -1,10 +1,15 @@
-function count_all_divisions(arr, n) {
-    let count = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % n == 0) {
-            count++
-        }
-    }
-    return count
-}
-console.log(count_all_divisions([1, 5, 7, 8], 1))
+// function count_all_divisions(arr, n) {
+//     let count = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % n == 0) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(count_all_divisions([1, 5, 7, 8], 1))
+
+function substring(str, a, b){
+   return str.slice(a,b)
+  }
+console.log(substring('aircampus', 3,9))
