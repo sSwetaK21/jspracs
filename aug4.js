51a8abbca83c187ea0a02bb59a8d987a5f7b1b09
shweta <shwetakodamg@gmail.com>
2022-08-04 23:44:36 +0530

    form

--- aug4.js
+++ aug4.js
@@ -7,6 +7,8 @@ bodyEl.append("Hello Shweta!", "Hey")
 let divEl = document.createElement("div")
 divEl.innerText = "hi div!"
 
+divEl.innerHTML = `<strong>this is strong tag</strong>`
+
 bodyEl.append(divEl)
 
 // body.appendChild(divEl)
@@ -16,3 +18,8 @@ let pEl = document.createElement("p")
 pEl.textContent = "Paragraph is here"
 
 divEl.append(pEl)
+
+let p1El = document.querySelector("#p1")
+let spanEl = document.querySelector(".span1")
+
+spanEl.remove()
