fa7b285df1ac99e2f518bef6e9bf72b1472b0b04
shweta <shwetakodamg@gmail.com>
2022-08-04 23:37:02 +0530

    form

--- aug4.js
+++ aug4.js
@@ -1,1 +1,18 @@
 //============js Basics=============
+
+let bodyEl = document.body
+// print.append("Hello Shweta!")
+bodyEl.append("Hello Shweta!", "Hey")
+
+let divEl = document.createElement("div")
+divEl.innerText = "hi div!"
+
+bodyEl.append(divEl)
+
+// body.appendChild(divEl)
+
+let pEl = document.createElement("p")
+// pEl.innerText = "Paragraph is here"
+pEl.textContent = "Paragraph is here"
+
+divEl.append(pEl)
